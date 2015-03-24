c24382d425e1a538ce52fc18480b98cbae6094f5
Sean Wesenberg <wookets@gmail.com>
2015-03-24 14:33:40 -0500

    inbetween commits

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('./'));
+app.use(express.static('public'));
 
 var server = app.listen(3000, function () {
   console.log('Example app listening at http://localhost:3000')
